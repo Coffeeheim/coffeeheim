7146848f031089a170b8998478badc5b12dd394b
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-07-03 15:17:39 -0300

    add itty router

--- app/index.js
+++ app/index.js
@@ -1,11 +1,20 @@
+import { Router } from "itty-router";
 import html from "./index.html";
 
-export default {
-  async fetch(request) {
-    return new Response(html, {
-      headers: {
-        "content-type": "text/html;charset=UTF-8",
-      },
-    });
-  },
-};
+const router = Router();
+
+router.get("/", () => {
+  return new Response(html, {
+    headers: {
+      "Content-Type": "text/html;charset=UTF-8",
+    },
+  });
+});
+
+router.all("*", () => {
+  return new Response("404, not found!", { status: 404 });
+});
+
+addEventListener("fetch", (e) => {
+  e.respondWith(router.handle(e.request));
+});
