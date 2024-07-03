1607f11a14aca6ec2ebc47c5820d22705331cdf1
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-07-03 18:16:59 -0300

    add websocket

--- app/functions/ws.js
+++ app/functions/ws.js
@@ -1,3 +1,16 @@
-export function onRequest(context) {
-  return new Response('Hello, world!')
+export function onRequest(request) {
+  const upgradeHeader = request.headers.get('Upgrade')
+  if (!upgradeHeader || upgradeHeader !== 'websocket') {
+    return new Response('Expected Upgrade: websocket', { status: 426 })
+  }
+
+  const webSocketPair = new WebSocketPair()
+  const [client, server] = Object.values(webSocketPair)
+
+  server.accept()
+
+  return new Response(null, {
+    status: 101,
+    webSocket: client,
+  })
 }
