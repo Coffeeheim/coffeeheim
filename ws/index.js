bfb8304b9b1201e95498750c5d94fa948c6220d1
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-07-03 19:37:58 -0300

    update

--- ws/index.js
+++ ws/index.js
@@ -1,11 +1,13 @@
 async function handleRequest(request) {
   const upgradeHeader = request.headers.get('Upgrade')
+
   if (!upgradeHeader || upgradeHeader !== 'websocket') {
     return new Response('Expected websocket', { status: 426 })
   }
 
   const [client, server] = Object.values(new WebSocketPair())
   server.accept()
+
   server.addEventListener('message', async ({ data }) => {
     console.log(data)
     server.send(JSON.stringify({ tz: new Date() }))
