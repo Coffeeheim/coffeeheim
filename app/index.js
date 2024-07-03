a02ca21ad5c63108c11fb6b1abb22ddc6fccfe7e
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-07-03 15:40:12 -0300

    add websocket

--- app/index.js
+++ app/index.js
@@ -11,4 +11,29 @@ router.get('/', () => {
   })
 })
 
+router.get('/ws', (req) => {
+  if (req.headers.get('Upgrade') !== 'websocket') {
+    return new Response('Expected websocket', { status: 400 })
+  }
+
+  const [client, websocket] = Object.values(new WebSocketPair())
+
+  websocket.accept()
+
+  websocket.send(JSON.stringify({ connected: true }))
+
+  websocket.addEventListener('message', async ({ data }) => {
+    websocket.send(JSON.stringify({ data }))
+  })
+
+  websocket.addEventListener('close', async (e) => {
+    console.log(e)
+  })
+
+  return new Response(null, {
+    status: 101,
+    webSocket: client,
+  })
+})
+
 export default { ...router }
