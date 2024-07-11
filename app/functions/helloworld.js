e8f35e2c46b7b0bdbfd9dca8c45f703048d69233
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-07-10 21:15:56 -0300

    testing env

--- app/functions/helloworld.js
+++ app/functions/helloworld.js
@@ -1,6 +1,6 @@
 import mqtt from 'mqtt'
 
-export function onRequest(request, env, ctx) {
+export function onRequest({ request, env }) {
   const MQTT_HOST = env.MQTT_HOST
   const MQTT_USERNAME = env.MQTT_USERNAME
   const MQTT_PASSWORD = env.MQTT_PASSWORD
