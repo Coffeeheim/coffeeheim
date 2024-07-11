ca668c78cb02b6ba641b25eb96e0e1335b99e722
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-07-10 21:21:18 -0300

    testing

--- app/functions/helloworld.js
+++ app/functions/helloworld.js
@@ -5,11 +5,11 @@ export function onRequest({ request, env }) {
   const MQTT_USERNAME = env.MQTT_USERNAME
   const MQTT_PASSWORD = env.MQTT_PASSWORD
 
-  const client = mqtt.connect(MQTT_HOST, {
-    username: MQTT_USERNAME,
-    password: MQTT_PASSWORD,
-  })
+  // const client = mqtt.connect(MQTT_HOST, {
+  //   username: MQTT_USERNAME,
+  //   password: MQTT_PASSWORD,
+  // })
 
-  client.publish('cluster/messages/node7', 'Hello, HiveMQ!')
+  // client.publish('cluster/messages/node7', 'Hello, HiveMQ!')
   return new Response('Hello,!')
 }
