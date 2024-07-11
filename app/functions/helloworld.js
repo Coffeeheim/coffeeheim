17a1e0e5dfc9f873eb97030edf905f87ddc69827
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-07-10 21:10:59 -0300

    fix env

--- app/functions/helloworld.js
+++ app/functions/helloworld.js
@@ -1,15 +1,15 @@
 import mqtt from 'mqtt'
 
-const MQTT_HOST = process.env.MQTT_HOST
-const MQTT_USERNAME = process.env.MQTT_USERNAME
-const MQTT_PASSWORD = process.env.MQTT_PASSWORD
+export function onRequest(request, env, ctx) {
+  const MQTT_HOST = env.MQTT_HOST
+  const MQTT_USERNAME = env.MQTT_USERNAME
+  const MQTT_PASSWORD = env.MQTT_PASSWORD
 
-const client = mqtt.connect(MQTT_HOST, {
-  username: MQTT_USERNAME,
-  password: MQTT_PASSWORD,
-})
+  const client = mqtt.connect(MQTT_HOST, {
+    username: MQTT_USERNAME,
+    password: MQTT_PASSWORD,
+  })
 
-export function onRequest(context) {
   client.publish('cluster/messages/node7', 'Hello, HiveMQ!')
-  return new Response('Hello, world!')
+  return new Response('Hello,!')
 }
