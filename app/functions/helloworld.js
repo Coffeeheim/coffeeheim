3ff5b3e2fe48ee6baa966f79a89992a7ee30c806
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-07-10 21:05:39 -0300

    add dep

--- app/functions/helloworld.js
+++ app/functions/helloworld.js
@@ -1,3 +1,15 @@
+import mqtt from 'mqtt'
+
+const MQTT_HOST = process.env.MQTT_HOST
+const MQTT_USERNAME = process.env.MQTT_USERNAME
+const MQTT_PASSWORD = process.env.MQTT_PASSWORD
+
+const client = mqtt.connect(MQTT_HOST, {
+  username: MQTT_USERNAME,
+  password: MQTT_PASSWORD,
+})
+
 export function onRequest(context) {
+  client.publish('cluster/messages/node7', 'Hello, HiveMQ!')
   return new Response('Hello, world!')
 }
