c46fdace832384f5c0781e1c9f0a9df204b3938e
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-07-31 15:05:53 -0300

    update exception

--- tail.py
+++ tail.py
@@ -1,7 +1,10 @@
 import json
+import logging
 import re
 import time
 from datetime import datetime
+
+logging.basicConfig(level=logging.DEBUG)
 
 bannedlist = 'valheim-server/config/bannedlist.txt'
 
@@ -32,10 +35,19 @@ def parse(lines):
 
 
 if __name__ == '__main__':
-    logfile = open('logs/buffer/buffer.b61debe1b38324213fd6eed25250283f0.log')
-    loglines = follow(logfile)
-    loglines = grep('debugmode', loglines)
-    loglines = parse(loglines)
+    try:
+        logging.info('Press CTRL+C to exit...')
 
-    for dt, j in loglines:
-        print(j['log'])
+        file = 'logs/buffer/buffer.b61debe1b38324213fd6eed25250283f0.log'
+        logfile = open(file)
+        loglines = follow(logfile)
+        loglines = grep('debugmode', loglines)
+        loglines = parse(loglines)
+
+        for dt, j in loglines:
+            print(j['log'])
+    except KeyboardInterrupt:
+        ...
+    except Exception as exc:
+        logging.info('Caught an Exception, something went wrong...')
+        logging.exception(exc)
